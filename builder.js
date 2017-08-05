e02d29c0bd3bac2e4274185fcdc390ffceed775a
Andrew S <andrews54757@gmail.com>
2017-08-05 00:49:41 -0400

    Update

--- builder.js
+++ builder.js
@@ -13,7 +13,92 @@ if (mm < 10) {
 }
 var date = dd + '/' + mm + '/' + yyyy;
 
-
+function removeComments(str) {
+    str = ('__' + str + '__').split('');
+    var mode = {
+        singleQuote: false,
+        doubleQuote: false,
+        regex: false,
+        blockComment: false,
+        lineComment: false,
+        condComp: false 
+    };
+    for (var i = 0, l = str.length; i < l; i++) {
+ 
+        if (mode.regex) {
+            if (str[i] === '/' && str[i-1] !== '\\') {
+                mode.regex = false;
+            }
+            continue;
+        }
+ 
+        if (mode.singleQuote) {
+            if (str[i] === "'" && str[i-1] !== '\\') {
+                mode.singleQuote = false;
+            }
+            continue;
+        }
+ 
+        if (mode.doubleQuote) {
+            if (str[i] === '"' && str[i-1] !== '\\') {
+                mode.doubleQuote = false;
+            }
+            continue;
+        }
+ 
+        if (mode.blockComment) {
+            if (str[i] === '*' && str[i+1] === '/') {
+                str[i+1] = '';
+                mode.blockComment = false;
+            }
+            str[i] = '';
+            continue;
+        }
+ 
+        if (mode.lineComment) {
+            if (str[i+1] === 'n' || str[i+1] === 'r') {
+                mode.lineComment = false;
+            }
+            str[i] = '';
+            continue;
+        }
+ /*
+        if (mode.condComp) {
+            if (str[i-2] === '@' && str[i-1] === '*' && str[i] === '/') {
+                mode.condComp = false;
+            }
+            continue;
+        }
+ */
+        mode.doubleQuote = str[i] === '"';
+        mode.singleQuote = str[i] === "'";
+ 
+        if (str[i] === '/') {
+            /*
+            if (str[i+1] === '*' && str[i+2] === '@') {
+                mode.condComp = true;
+                continue;
+            }
+            */
+            if (str[i+1] === '*') {
+                str[i] = '';
+                mode.blockComment = true;
+                continue;
+            }
+            if (str[i+1] === '/') {
+                str[i] = '';
+                mode.lineComment = true;
+                continue;
+            }
+            mode.regex = true;
+ 
+        }
+ 
+    }
+    
+    return str.join('').slice(2, -2).replace(/\n\s*\n/g,"\n");
+}
+            
 var fs = require("fs");
 
 var simple = fs.readFileSync(__dirname + "/lib/parser/Simple.php", "utf8");
@@ -25,10 +110,13 @@ var connector = fs.readFileSync(__dirname + "/lib/connector/index.php", "utf8");
 var main = fs.readFileSync(__dirname + "/index.php", "utf8");
 
 var startstr = "// BUILD BETWEEN";
-simple = simple.split(startstr)[1];
-adv = adv.split(startstr)[1];
-connector = connector.split(startstr)[1];
-main = main.split(startstr)[1];
+
+
+
+simple = removeComments(simple.split(startstr)[1]);
+adv = removeComments(adv.split(startstr)[1]);
+connector = removeComments(connector.split(startstr)[1]);
+main = removeComments(main.split(startstr)[1]);
 
 var out = `<?php\n\
 /*\n\
--- dist/SuperSQL.php
+++ dist/SuperSQL.php
@@ -39,13 +39,8 @@ class Response
     public $ind;
     public $error;
     public $errorData;
-    
-   /**
-    * Gets data from a query
-    */
     function __construct($data, $error)
     {
-        
         $this->error = !$error;
         if (!$error) {
             $this->errorData = $data->errorInfo();
@@ -54,113 +49,61 @@ class Response
             $this->affected = $data->rowCount();
         }
         $this->ind = 0;
-        
         $data->closeCursor();
     }
-    
-   /**
-    * Returns error data if there is one
-    */
     function error()
     {
         return $this->error ? $this->errorData : false;
     }
-
-   /**
-    * Gets data from the query
-    *
-    * @returns {Array}
-    */
     function getData()
     {
         return $this->result;
     }
-    
-   /**
-    * Gets number of affected rows from the query
-    *
-    * @returns {Int}
-    */
     function getAffected() {
         return $this->affected;
     }
-    
-   /**
-    * Gets next row
-    */
     function next()
     {
         return $this->result[$this->ind++];
     }
-    
-   /**
-    * Resets ititerator
-    */
     function reset()
     {
         $this->ind = 0;
     }
-    
 }
-
 class Connector
 {
     public $queries = [];
     public $db;
     public $log = [];
     public $dev = false;
-    
-   /**
-    * Creates a connection
-    * @param {String} dsn - DSN of the connection
-    * @param {String} user - Username
-    * @param {String} pass - Password
-    */
     function __construct($dsn, $user, $pass)
     {
         $this->db = new \PDO($dsn, $user, $pass);
         $this->log = array();
     }
-    
-   /**
-    * Queries database
-    * @param {String} query - Query to make
-    *
-    * @returns {SQLResponse}
-    */
     function query($query)
     {
         $q = $this->db->prepare($query);
         $q->execute();
-        
         if ($this->dev) array_push($this->log,[$query]);
         return new Response($q);
     }
-    
-   /**
-    * Queries database efficiently
-    * @param {String} sql - Base query
-    * @param {Array} insert - array of args
-    *
-    * @returns {SQLResponse|SQLResponse[]}
-    */
     function _query($sql, $insert)
     {
-         // echo json_encode(array($sql,$insert));
-         // return;
-        if (isset($this->queries[$sql])) { // Cache
-            $q = $this->queries[$sql];
+         n_encode(array($sql,$insert));
+         return;
+        if (isset($this->queries[$sql])) { ries[$sql];
             if ($this->dev) array_push($this->log,["fromcache",$sql,$insert]);
         } else {
             $q             = $this->db->prepare($sql);
             $this->queries[$sql] = $q;
             if ($this->dev) array_push($this->log,[$sql,$insert]);
         }
-        
-        if (count($insert) == 1) { // Single query
+        if (count($insert) == 1) { ngle query
             $e = $q->execute($insert[0]);
             return new Response($q, $e);
-        } else { // Multi Query
+        } else { ry
             $responses = array();
             foreach ($insert as $key => $value) {
                 $e = $q->execute($insert[0]);
@@ -169,19 +112,11 @@ class Connector
             return responses;
         }
     }
-    
-   /**
-    * Closes the connection
-    */
     function close()
     {
         $this->db = null;
         $this->queries = null;
-        
     }
-   /**
-    * Clears cache
-    */
     function clear()
     {
         $this->queries = [];   
@@ -195,7 +130,6 @@ class SimpleParser {
         $sql .= " WHERE ";
         $i = 0;
         foreach ($where as $key => $value) {
-        
             if ($i != 0) {
                 $sql .= " AND ";
             }
@@ -209,34 +143,26 @@ class SimpleParser {
         $sql = "SELECT ";
         $insert = array(array());
         $len = count($columns);
-        if ($len == 0) { // none
+        if ($len == 0) { none
             $sql .= "*";
-        } else { // some
-           for ($i = 0; $i < $len; $i++) {
+        } else { r ($i = 0; $i < $len; $i++) {
                     if ($i != 0) {
                         $sql .= ", ";
                     }
                     $sql .= "`" . $columns[$i] . "`";
                 }
         }
-        
         $sql .= "FROM `" . $table . "`";
-        
         self::WHERE($where,$sql,$insert);
-        
         $sql .= " " . $append;
-        
         return array($sql,$insert);
     }
     public static function INSERT($table,$data) {
         $sql = "INSERT INTO `" . $table . "` (";
         $add = ") VALUES (";
         $insert = array(array());
-        
         $i = 0;
-        
         foreach ($data as $key => $value) {
-        
             if ($i != 0) {
                 $sql .= ", ";
                 $add .= ", ";
@@ -246,15 +172,12 @@ class SimpleParser {
             array_push($insert[0],$value);
             $i++;
         }
-        
         $sql .= $add;
-        
         return array($sql,$insert);
     }
     public static function UPDATE($table,$data,$where) {
         $sql = "UPDATE `" . $table . "` SET ";
         $insert = array(array());
-        
         $i = 0;
         foreach ($data as $key => $value) {
             if ($i != 0) {
@@ -264,14 +187,12 @@ class SimpleParser {
            array_push($insert[0],$value);
                $i++;
         }
-        
         self::WHERE($where,$sql,$insert);
         return array($sql,$insert);
     }
     public static function DELETE($table,$where) {
         $sql = "DELETE FROM `" . $table . "`";
         $insert = array(array());
-        
         self::WHERE($where,$sql,$insert);
         return array($sql,$insert);
     }
@@ -280,13 +201,6 @@ class SimpleParser {
 // lib/parser/Advanced.php
 class AdvancedParser
 {
-   /**
-    * Reads argument data from a string
-    *
-    * @param {String} str - String to read from
-    *
-    * @returns {String|Boolean}
-    */
     private static function parseArg(&$str)
     {
         if (substr($str, 0, 1) == "[") {
@@ -297,35 +211,18 @@ class AdvancedParser
             return false;
         }
     }
-    
-   /**
-    * Reads argument data from a string
-    *
-    * @param {String} str - String to read from
-    *
-    * @returns {String|Boolean}
-    */
     private static function parseArgs(&$str)
     {
         $arr = array();
-        
         for ($i = 0; $i < 5; $i++) {
-            
         if (substr($str, 0, 1) == "[") {
           array_push($arr,substr($str, 1, 3));
          $str = substr($str,4);
         } else {
             return $arr;
         }
-            
         }
     }
-   /**
-    * Appends value(s) to arguments
-    *
-    * @param {&Array} args - Arguments to append to
-    * @param {String|Int|Boolean|Array} val - Value(s) to append
-    */
     private static function append(&$args, $val)
     {
         $type = gettype($val);
@@ -351,15 +248,6 @@ class AdvancedParser
             }
         }
     }
-    
-   /**
-    * Appends values to arguments
-    *
-    * @param {&Array} insert - Arguments to append to
-    * @param {Object} indexes - Indexes of keys
-    * @param {Array} dt - Values to append
-    */
-    
     private static function append2(&$insert, $indexes, $dt)
     {
         function recurse(&$holder,$val,$indexes,$par,$lvl) {
@@ -390,53 +278,30 @@ class AdvancedParser
                 }
             }
         }
-        
         $last = false;
         foreach ($dt as $key => $val) {
             if (!isset($insert[$key]))
                 $insert[$key] = array();
             $holder = $last ? array_slice($last, 0) : array();
-            
             recurse($holder,$val,$indexes,"",0);
             if (!$last)
                 $last = $holder;
             $c = count($holder);
-            
             for ($i = 0; $i < $c; $i++) {
                 array_push($insert[$key], $holder[$i]);
             }
         }
-        
     }
-    
-   /**
-    * Constructs logical conditional statements
-    *
-    * @param {Object|Array} arr - Conditions
-    * @param {Array} args
-    *
-    * @returns {String}
-    */
     private static function conditions($arr, &$args, $quotes = true)
     {
-     
         $cond = function(&$cond,&$arr,&$args,$quotes,$statement,$default,&$indexes,&$i,$lvl,$parent,$append = false) {
-            
             $b = 0;
-            
             foreach ($arr as $key => $value) {
-                
-                
                 $arg = self::parseArg($key);
-                
                 $type = gettype($value);
-                
                 $s = $statement;
-                
                 $o = $default;
-                
                 $useBind = false;
-                
                 if ($type == "array") {
                 switch ($arg) {
                     case "&&]":
@@ -451,19 +316,15 @@ class AdvancedParser
                          break;
                     }
                 }
-                
                     switch ($arg) {
                 case ">>]":
                     $o = " > ?";
-             
                     break;
                 case "<<]":
                     $o = " < ?";
-            
                     break;
                 case ">=]":
                     $o = " >= ?";
-                
                     break;
                 case "<=]":
                     $o = " <= ?";
@@ -472,9 +333,7 @@ class AdvancedParser
                 if ($useBind) $o = " = ?";
                     break;
                     }
-                
                 if ($b != 0) $sql .= $statement;
-                
                 if ($type == "array") {
                   if ($useBind) {
                     $sql .= "(" .$cond($cond,$value,$args,$quotes,$s,$o,$indexes,$i,$lvl + 1,$key,$append) . ")";
@@ -508,11 +367,8 @@ class AdvancedParser
                 }
                 $b++;
                 }
-            
               return $sql;
-            
         };
-        
 $indexes = array();
      $i = 0;
       if (isset($arr[0])) {
@@ -523,36 +379,16 @@ $indexes = array();
       }
         return $sql;
     }
-    
-   /**
-    * Constructs SQL commands (SELECT)
-    *
-    * @param {String} table - SQL Table
-    * @param {Array} columns - Columns to return
-    * @param {Object|Array} where - Where clause
-    * @param {Object|null} join - Join clause 
-    * @param {Int} limit - Limit clause
-    * 
-    * @returns {Array}
-    */
     static function SELECT($table, $columns, $where, $join, $limit)
     {
-        
         $sql = "SELECT ";
-        
         $len = count($columns);
-        
         $insert = array(
             array()
         );
-        
-        
-        
-        if ($len == 0) { // none
+        if ($len == 0) { none
             $sql .= "*";
-        } else { // some
-            $i = 0;
-            $req = 0;
+        } else { req = 0;
             $into = "";
             if ($columns[0] == "DISTINCT") {
                 $i = 1;
@@ -567,77 +403,51 @@ $indexes = array();
                 $req = 1;
                 $into = " " . $columns[0] . " ";
             }
-            
-            if ($len > $req) { // has var
-                
+            if ($len > $req) { r
                 for (; $i < $len; $i++) {
-                    
                     if ($i > $req) {
                         $sql .= ", ";
                     }
                     $sql .= "`" . $columns[$i] . "`";
                 }
-                
             } else
                 $sql .= "*";
-            
             $sql .= $into;
         }
-        
         $sql .= " FROM `" . $table . "`";
-        
         if ($join) {
             foreach ($join as $key => $val) {
                 $arg = self::parseArg($key);
                 switch ($arg) {
                     case "<<]":
-                       
                         $sql .= " RIGHT JOIN ";
                         break;
                     case ">>]":
-                  
                         $sql .= " LEFT JOIN ";
                         break;
                     case "<>]":
-                    
                         $sql .= " FULL JOIN ";
                         break;
-                    default: // inner join
-                        
+                    default: nner join
                         $sql .= " JOIN ";
                         break;
                 }
-                
                 $sql .= "`" . $key . "` ON ";
-                
                 $c = self::conditions($val, $insert,false);
                 $sql .= $c;
             }
         }
-    
         if (count($where) != 0) {
             $sql .= " WHERE ";
             $c = self::conditions($where, $insert);
             $sql .= $c;
         }
-        
        if ($limit) $sql .= " LIMIT " . $limit;
-        
         return array(
             $sql,
             $insert
         );
-        
     }
-    
-   /**
-    * Constructs SQL commands (INSERT INTO)
-    *
-    * @param {String} table - SQL Table
-    * @param {Object|Array} data - Data to insert
-    *
-    * @returns {Array}
-    */
     static function INSERT($table, $data)
     {
         $sql    = "INSERT INTO `" . $table . "` (";
@@ -645,9 +455,7 @@ $indexes = array();
             array()
         );
         $values = "";
-        
         $i = 0;
-        
         if (isset($data[0])) {
             $indexes = array();
             foreach ($data[0] as $key => $val) {
@@ -669,35 +477,21 @@ $indexes = array();
                 $sql .= "`" . $key . "`";
                 $values .= "?";
                 $i++;
-                
                 self::append($insert, $val);
             }
         }
-        
         $sql .= ") VALUES (" . $values . ")";
-        
         return array(
             $sql,
             $insert
         );
     }
-    
-   /**
-    * Constructs SQL commands (UPDATE)
-    *
-    * @param {String} table - SQL Table
-    * @param {Object|Array} data - Data to update
-    * @param {Object|Array} where - Where clause
-    *
-    * @returns {Array}
-    */
     static function UPDATE($table, $data, $where)
     {
         $sql    = "UPDATE `" . $table . "` SET ";
         $insert = array(
             array()
         );
-        
         $i = 0;
         if (isset($data[0])) {
             $indexes = array();
@@ -719,7 +513,6 @@ $indexes = array();
                 self::append($insert, $val);
             }
         }
-        
         if (count($where) != 0) {
             $sql .= " WHERE ";
             $c = self::conditions($where, $insert);
@@ -730,22 +523,12 @@ $indexes = array();
             $insert
         );
     }
-    
-   /**
-    * Constructs SQL commands (DELETE)
-    *
-    * @param {String} table - SQL Table
-    * @param {Object|Array} where - Where clause
-    *
-    * @returns {Array}
-    */
     static function DELETE($table, $where)
     {
         $sql    = "DELETE FROM `" . $table . "`";
         $insert = array(
             array()
         );
-        
         if (count($where) != 0) {
             $sql .= " WHERE ";
             $c = self::conditions($where, $insert);
@@ -756,37 +539,16 @@ $indexes = array();
             $insert
         );
     }
-    
-    
 }
 
 // index.php
 class SuperSQL
 {
     public $connector;
-    
-   /**
-    * Creates a connection
-    * @param {String} dsn - DSN of the connection
-    * @param {String} user - Username
-    * @param {String} pass - Password
-    */
     function __construct($dsn, $user, $pass)
     {
         $this->connector = new Connector($dsn, $user, $pass);
     }
-    
-   /**
-    * Queries a SQL table (SELECT)
-    *
-    * @param {String} table - SQL Table
-    * @param {Array} columns - Columns to return
-    * @param {Object|Array} where - Where clause
-    * @param {Object|null} join - Join clause 
-    * @param {String} limit - Limit clause 
-    *
-    * @returns {SQLResponse|SQLResponse[]}
-    */
     function SELECT($table, $columns, $where, $join = null, $limit = false)
     {
      if (gettype($join) == "integer") {
@@ -796,136 +558,52 @@ class SuperSQL
         $d = Parser::SELECT($table, $columns, $where, $join, $limit);
         return $this->connector->_query($d[0], $d[1]);
     }
-    
-   /**
-    * Inserts data into a SQL table
-    *
-    * @param {String} table - SQL Table
-    * @param {Object|Array} data - Data to insert
-    *
-    * @returns {SQLResponse|SQLResponse[]}
-    */
     function INSERT($table, $data)
     {
         $d = Parser::INSERT($table, $data);
         return $this->connector->_query($d[0], $d[1]);
     }
-    
-   /**
-    * Updates a SQL table
-    *
-    * @param {String} table - SQL Table
-    * @param {Object|Array} data - Data to update
-    * @param {Object|Array} where - Where clause
-    *
-    * @returns {SQLResponse|SQLResponse[]}
-    */
     function UPDATE($table, $data, $where)
     {
         $d = Parser::UPDATE($table, $data, $where);
         return $this->connector->_query($d[0], $d[1]);
     }
-    
-   /**
-    * Deletes from a SQL table
-    *
-    * @param {String} table - SQL Table
-    * @param {Object|Array} where - Where clause
-    *
-    * @returns {SQLResponse|SQLResponse[]}
-    */
     function DELETE($table, $where)
     {
         $d = Parser::DELETE($table, $where);
         return $this->connector->_query($d[0], $d[1]);
     }
-    
-   /**
-    * Queries a SQL table (SELECT) (Simple)
-    *
-    * @param {String} table - SQL Table
-    * @param {Array} columns - Columns to return
-    * @param {Object} where - Where clause
-    * @param {String} append - SQL append
-    *
-    * @returns {SQLResponse}
-    */
     function sSELECT($table, $columns, $where, $append = "")
     {
         $d = Simple::SELECT($table, $columns, $where, $append);
         return $this->connector->_query($d[0], $d[1]);
     }
-    
-   /**
-    * Inserts data into a SQL table (Simple)
-    *
-    * @param {String} table - SQL Table
-    * @param {Object} data - Data to insert
-    *
-    * @returns {SQLResponse}
-    */
     function sINSERT($table, $data)
     {
         $d = Simple::INSERT($table, $data);
         return $this->connector->_query($d[0], $d[1]);
     }
-    
-   /**
-    * Updates a SQL table (Simple)
-    *
-    * @param {String} table - SQL Table
-    * @param {Object} data - Data to update
-    * @param {Object} where - Where clause
-    *
-    * @returns {SQLResponse}
-    */
     function sUPDATE($table, $data, $where)
     {
         $d = Simple::UPDATE($table, $data, $where);
         return $this->connector->_query($d[0], $d[1]);
     }
-    
-   /**
-    * Deletes from a SQL table (Simple)
-    *
-    * @param {String} table - SQL Table
-    * @param {Object} where - Where clause
-    *
-    * @returns {SQLResponse}
-    */
     function sDELETE($table, $where)
     {
         $d = Simple::DELETE($table, $where);
         return $this->connector->_query($d[0], $d[1]);
     }
-    
-   /**
-    * Query
-    */
     function query($query) {
         return $this->connector->query($query);
     }
-   /**
-    * Closes the connection
-    */
     function close() {
         $this->connector->close();
     }
-    
-   /**
-    * Turns on dev mode
-    */
     function dev() {
         $this->connector->dev = true;
     }
-    
-   /**
-    * Get log
-    */
     function getLog() {
         return $this->connector->log;
     }
-        
-    
 }
 ?>
