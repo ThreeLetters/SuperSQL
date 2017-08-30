e288d3182eda9672d625c072a4cc5491a5213532
Andrew S <andrews54757@gmail.com>
2017-08-30 10:32:50 -0400

    Add autoload.php

--- SuperSQL/helper.php
+++ SuperSQL/helper.php
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 namespace SuperSQL;
-use SuperSQL\lib/Parser as Parser;
+use SuperSQL\lib\Parser as Parser;
 // BUILD BETWEEN
 class SQLHelper
 {
---
+++ autoload.php
@@ -0,0 +1,6 @@
+<?php
+include 'SuperSQL/SuperSQL.php';
+include 'SuperSQL/lib/parser.php';
+include 'SuperSQL/lib/connector.php';
+include 'SuperSQL/helper.php';
+?>
